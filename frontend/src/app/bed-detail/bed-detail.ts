47d11300a034890c78380379cf272111ecf51001
frzbonney <bastianklumpp11@gmail.com>
2026-09-09 17:12:47 +0200

    feat: connect bed-detail to bed.service

--- frontend/src/app/bed-detail/bed-detail.ts
+++ frontend/src/app/bed-detail/bed-detail.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BedService, BedDetail as BedData } from '../bed.service';
 
 @Component({
   selector: 'app-bed-detail',
@@ -6,4 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './bed-detail.html',
   styleUrl: './bed-detail.css',
 })
-export class BedDetail {}
+export class BedDetail implements OnInit {
+  // ask Angular for the services and tools this component needs
+  private route = inject(ActivatedRoute);
+  private bedService = inject(BedService);
+  private changeDetector = inject(ChangeDetectorRef);
+
+  // the bed shown here, null until the request comes back
+  bed: BedData | null = null;
+
+  ngOnInit() {
+    // read the :id part of the current URL
+    const idText = this.route.snapshot.paramMap.get('id');
+    const id = Number(idText);
+
+    this.bedService.getBed(id).subscribe((bed) => {
+      this.bed = bed;
+      this.changeDetector.detectChanges();
+    });
+  }
+}
