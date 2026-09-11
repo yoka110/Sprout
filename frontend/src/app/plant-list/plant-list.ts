9f13f80aca6a7e5c1d04a20eb00481cc93f072a8
yousseffahmy220-ux <youssef.fahmy220@gmail.com>
2026-09-11 19:38:03 +0200

    refactor: use the same user id check in plant list as in bed detail

--- frontend/src/app/plant-list/plant-list.ts
+++ frontend/src/app/plant-list/plant-list.ts
@@ -20,7 +20,7 @@ export class PlantList implements OnInit {
 
   ngOnInit(): void {
     const currentUser = this.authService.getCurrentUser();
-    this.plantService.getPlants(currentUser?.id).subscribe({
+    this.plantService.getPlants(currentUser ? currentUser.id : undefined).subscribe({
       next: (plants) => {
         this.plants = plants;
         this.isLoading = false;
@@ -28,10 +28,10 @@ export class PlantList implements OnInit {
         this.changeDetector.detectChanges();
       },
       error: () => {
-        this.errorMessage = 'Die Pflanzen konnten nicht geladen werden.'
+        this.errorMessage = 'Die Pflanzen konnten nicht geladen werden.';
         this.isLoading = false;
         this.changeDetector.detectChanges();
-      }
+      },
     });
   }
 }
